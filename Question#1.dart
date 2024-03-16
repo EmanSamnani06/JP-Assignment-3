205091b703923a009d05aa4b4a79b0f680eac98b
EmanSamnani06 <emansamnani@gmail.com>
2024-03-17 00:43:33 +0500

    Checking number type

---
+++ Question#1.dart
@@ -0,0 +1,12 @@
+void main() {
+  //You can change the number as per your choice
+  int number = -2;
+  //Checking if  the number is positive, negative, or zero
+  if (number > 0) {
+    print("The number is Positive.");
+  } else if (number < 0) {
+    print("The number is Negative.");
+  } else {
+    print("The number is Zero.");
+  }
+}
